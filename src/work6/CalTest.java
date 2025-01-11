68ac9ca2be5b7e62c62b053ba140e4aee2bf4a41
FengTing <paul22455370@gmail.com>
2025-01-11 15:33:30 +0800

    fourth commit revise

--- src/work6/CalTest.java
+++ src/work6/CalTest.java
@@ -1,42 +1,10 @@
 package work6;
 
-import java.util.Scanner;
-
 public class CalTest {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
 		Calculator myCal = new Calculator();
-		int x , y;
-
-		testCal: while (true) {
-			System.out.println("請輸入x的值");
-			try {
-				x = sc.nextInt();
-			} catch (Exception e) {
-				System.out.println("輸入格式不正確，請再輸入一次");
-				sc.nextLine();
-				continue testCal;
-			}
-			verY: while (true) {
-				System.out.println("請輸入y的值");
-				try {
-					y = sc.nextInt();
-				} catch (Exception e) {
-					System.out.println("輸入格式不正確，請再輸入一次");
-					sc.nextLine();
-					continue verY;
-				}
-				break;
-			}
-			try {
-				System.out.println(myCal.powerXY(x, y));
-			} catch (CalException e) {
-				continue testCal;
-			}
-			break;
-		}
-		sc.close();
+		myCal.powerXY();
 	}
-
 }
--- src/work6/Calculator.java
+++ src/work6/Calculator.java
@@ -1,17 +1,48 @@
 package work6;
 
+import java.util.Scanner;
+
 public class Calculator {
 
-	public int powerXY(int x, int y) throws CalException {
-		if (x == 0 && y == 0) {
-			System.out.println("0的0次方沒有意義!");
-			throw new CalException();
-		} else if (y < 0) {
-			System.out.println("次方為負值，結果回傳不為整數!");
-			throw new CalException();
-		} else {
-			int sum = (int) Math.pow(x, y);
-			return sum;
+	public void powerXY() {
+		Scanner sc = new Scanner(System.in);
+		int sum, x, y;
+
+		verX: while (true) {
+			System.out.println("請輸入x的值");
+			try {
+				x = sc.nextInt();
+			} catch (Exception e) {
+				System.out.println("x值的輸入格式不正確，請再輸入一次");
+				sc.nextLine();
+				continue verX;
+			}
+			verY: while (true) {
+				System.out.println("請輸入y的值");
+				try {
+					y = sc.nextInt();
+				} catch (Exception e) {
+					System.out.println("y值的輸入格式不正確，請再輸入一次");
+					sc.nextLine();
+					continue verY;
+				}
+				break;
+			}
+			try {
+				if (x == 0 && y == 0) {
+					System.out.println("0的0次方沒有意義!");
+					throw new CalException();
+				} else if (y < 0) {
+					System.out.println("次方為負值，結果回傳不為整數!");
+					throw new CalException();
+				} else {
+					sum = (int) Math.pow(x, y);
+					System.out.println(x + "的" + y + "次方等於" + sum);
+				}
+			} catch (CalException e) {
+				continue verX;
+			}
+			break;
 		}
 	}
 }
